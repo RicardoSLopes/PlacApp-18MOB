0c15e02d50d675fa37605930ac1c915ca52bc835
Logon Aluno <logonaluno@local.com>
2019-08-24 10:55:37 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/ui/game/GameViewModel.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.ui.game
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel(){
+    var eventName : String = "" // para inicializar com nulo String?
+    var homeTeam = ""
+    var awayTeam = ""
+
+}
+
+
